4433a551f91d05e8c0c4ffffd389878495c96c69
Lee <joshbeckhamlee.sabulao@gmail.com>
2026-09-19 18:17:02 +0800

    i added

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Ayoko na");
+        int a = 15; int b = 2;
     }
 }
