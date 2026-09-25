9a3550cc885c9fe0cc656e9b5970b48125463f9d
Josh <joshrodenbermundobanot@gmail.com>
2026-09-25 17:41:08 +0800

    yayayaya

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        int xx = 0; int yy = 11;
-        xx += yy;
-        System.out.println(xx);
+
+
+        System.out.println("pag nakita mo to patay ka na");
     }
 }
