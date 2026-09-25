60996033ca5bff77588a05e320832afd53f99d9e
Lee <joshbeckhamlee.sabulao@gmail.com>
2026-09-25 17:10:26 +0800

    added var

--- src/App.java
+++ src/App.java
@@ -1,11 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Ayoko na");
-<<<<<<< HEAD
-        int a = 10;
-=======
-        int a = 15; int b = 2;
->>>>>>> main
+        int x = 0;
     }
 }
