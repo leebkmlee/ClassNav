de43620376ce1ee56c112aa877d4357ea7d8bb6e
Josh <joshrodenbermundobanot@gmail.com>
2026-09-25 17:21:28 +0800

    bagong code

--- src/App.java
+++ src/App.java
@@ -1,7 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-
         int xx = 0; int yy = 11;
+        xx += yy;
+        System.out.println(xx);
     }
 }
