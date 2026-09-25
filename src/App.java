0c6b00c4f1cc60833df59ff50a380f221629879e
Lee <joshbeckhamlee.sabulao@gmail.com>
2026-09-25 18:56:27 +0800

    tanginamo banot maligo ka na

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Hello");
+        System.out.println("Hello, Putangina ayaw ko na");
     }
 }
