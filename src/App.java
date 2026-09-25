380ca5e5cee2785646a43ed6fcae523635cad771
Josh <joshrodenbermundobanot@gmail.com>
2026-09-25 18:52:02 +0800

    ma

--- src/App.java
+++ src/App.java
@@ -1,9 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("pag nakita nyo to, maguupdate remote nyo");
-        System.out.println("hi");
-
         System.out.println("NEGRANG NEGRA SI ELIE");
     }
 }
