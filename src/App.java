cb26992902e90e5328ee8e31f88df07c34b65834
Josh <joshrodenbermundobanot@gmail.com>
2026-09-25 19:06:27 +0800

    mama

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("NEGRANG NEGRA SI ELIE");
 
-        System.out.println("hi hi hi");
+        System.out.println("hi hi hi hi");
     }
 }
