bd0a87c7f9beafd757a04799f97029bcae67bd7a
Lee <joshbeckhamlee.sabulao@gmail.com>
2026-09-25 17:32:36 +0800

    commit add

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         int x = 0; int y = 1;
+        System.out.println("Hello");
     }
 }
