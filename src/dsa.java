0d3488546004326b0357d8e012a763291bb2be29
Lee <joshbeckhamlee.sabulao@gmail.com>
2026-09-25 16:56:18 +0800

    hi

---
+++ src/dsa.java
@@ -0,0 +1,8 @@
+public class dsa {
+    public static void main (String [] args) {
+        System.out.println("hi");
+        for (int x = 0; x < 5; x++){
+            System.out.println("hi + " + x);
+        }
+    }
+}
