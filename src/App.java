7ac79b5e377d8a38d9ead680a9fc3bf141460d64
Josh <joshrodenbermundobanot@gmail.com>
2026-09-25 17:17:43 +0800

    whats up tea myey

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        int xx = 0; int yy = 1;
+        int xx = 0; int yy = 11;
     }
 }
